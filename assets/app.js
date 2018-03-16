66509ef01c4c17b5a90432a911ae10a658027420
Joseph Vega <1josephvega@gmail.com>
2018-03-15 20:38:20 -0400

    everything working, just need to add date / total billed calculations

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,9 @@
 var config = {
     apiKey: "AIzaSyDTizygWTD0GAM5UJwD9-jzkA1mtlegFmk",
     authDomain: "employee-data-management-594a8.firebaseapp.com",
-    databaseURL: "https://employee-data-management-594a8.firebaseio.com",
+    databaseURL: "https://employee-data-management-594a8.firebaseio.com/",
     storageBucket: "employee-data-management-594a8.appspot.com",
-    messagingSenderId: "208476116054"
+    // messagingSenderId: "208476116054"
   };
 
   firebase.initializeApp(config);
@@ -16,3 +16,46 @@ var config = {
    var role = "";
    var startDate = 0;
    var monthlyRate = "";
+
+  // Capture Button Click
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    // Grabbed values from text boxes
+    name = $("#name").val().trim();
+    role = $("#role").val().trim();
+    startDate = $("#date").val().trim();
+    monthlyRate = $("#rate").val().trim();
+
+    // Code for handling the push
+    database.ref().push({
+      name: name,
+      role: role,
+      startDate: startDate,
+      monthlyRate: monthlyRate,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+  });
+
+  // Firebase watcher + initial loader + order/limit HINT: .on("child_added"
+  database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Console.loging the last user's data
+    console.log(sv.name);
+    console.log(sv.role);
+    console.log(sv.startDate);
+    console.log(sv.monthlyRate);
+
+    // Change the HTML to reflect
+    $("#name-display").text(sv.name);
+    $("#role-display").text(sv.role);
+    $("#startDate-display").text(sv.startDate);
+    $("#monthlyRate-display").text(sv.monthlyRate);
+
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
